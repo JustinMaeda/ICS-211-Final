ddd15d0f5bce3901dde8ce2a641d2343d5339e54
JustinMaeda <jkmaeda@hawaii.edu>
2024-12-05 13:13:36 -1000

    Added Comment class in models

--- tasks/models.py
+++ tasks/models.py
@@ -25,6 +25,16 @@ class Task(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username}"
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
