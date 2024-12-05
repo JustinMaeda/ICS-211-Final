2042b8b0ceff8a37ce9972c4c1dcdde2addc7a00
JustinMaeda <jkmaeda@hawaii.edu>
2024-12-04 21:32:19 -1000

    Create Forms
    Added a user registration form and a Profile form.

---
+++ tasks/forms.py
@@ -0,0 +1,12 @@
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio',]
