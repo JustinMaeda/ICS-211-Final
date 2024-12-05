0d40c84c50a9609613dfa965e5194eefaf76f7ad
JustinMaeda <jkmaeda@hawaii.edu>
2024-12-04 21:31:38 -1000

    Added Profile class in models.py
    Also added functions to create and save user profiles.

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
@@ -21,3 +23,22 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
