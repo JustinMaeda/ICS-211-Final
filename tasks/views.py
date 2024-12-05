aadeb49497dcd0177d2e1e9d8dd2df6fa7c4c9d9
JustinMaeda <jkmaeda@hawaii.edu>
2024-12-04 21:44:25 -1000

    Added Profiles views
    Login. Logout, Register, View, and Edit views made to facilitate User profiles and all associated functions.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('thread_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'forum_website/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('thread_list')
+    return render(request, 'forum_website/login.html')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def profile_view(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    return render(request, 'forum_website/profile_view.html', {'profile': profile})
+
+@login_required
+def edit_profile(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_view')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'forum_website/edit_profile.html', {'form': form})
+
