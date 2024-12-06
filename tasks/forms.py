5fd959230fcd7ad3af8142f82b2e5e46dbba4d95
JustinMaeda <jkmaeda@hawaii.edu>
2024-12-05 14:07:58 -1000

    Added a comment form

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Project, Profile, Task
+from .models import Project, Profile, Task, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -26,4 +26,13 @@ class ProjectForm(forms.ModelForm):
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date']
+        fields = ['title', 'description', 'due_date', 'assigned_to', 'status', 'project']
+
+    assigned_to = forms.ModelChoiceField(queryset=User.objects.all(), required=True)
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=True)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # Only need the content for a comment
